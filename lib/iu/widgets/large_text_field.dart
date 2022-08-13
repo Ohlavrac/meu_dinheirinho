3ab049fc2f08f9f3b98dd9b397e0f173dcedb44c
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-08-13 15:36:09 -0300

    Adicionado widget de texto

---
+++ lib/iu/widgets/large_text_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../colors/app_colors.dart';
+import '../texts/app_texts.dart';
+
+class LargeTextField extends StatelessWidget {
+  final String label;
+  final bool isNumberInput;
+  final double width;
+  const LargeTextField({Key? key, required this.label, required this.isNumberInput, required this.width}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: TextField(
+        keyboardType: isNumberInput == true ? TextInputType.number : TextInputType.text,
+        style: AppTexts.simple,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          labelStyle: AppTexts.simple,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(0),
+            borderSide: BorderSide(color: AppColors.textBlack, width: 1.5)
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(0),
+            borderSide: BorderSide(color: AppColors.textBlack, width: 1.5)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(0),
+            borderSide: BorderSide(color: AppColors.primary, width: 1.5)
+          ),
+          labelText: label,
+        ),
+      ),
+    );
+  }
+}
