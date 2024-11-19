03e199c1fd3688df2afdab337f96ea0fe11f43a9
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-11-19 09:48:16 -0300

    feat: add moviment DTO

---
+++ lib/data/dtos/moviments_dto.dart
@@ -0,0 +1,27 @@
+class MovimentsDto {
+  int? id;
+  late String title;
+  late double amount;
+  DateTime? createdAt;
+  late bool type;
+  late String category;
+  late bool repeat;
+  late int repeatMonths;
+  late String monthYearString;
+  late String lastMonthYearString;
+  DateTime? lastMonthYear;
+
+  MovimentsDto({
+    this.id,
+    required this.title,
+    required this.amount,
+    this.createdAt,
+    required this.type,
+    required this.category,
+    required this.repeat,
+    required this.repeatMonths,
+    required this.monthYearString,
+    required this.lastMonthYearString,
+    this.lastMonthYear
+  });
+}
