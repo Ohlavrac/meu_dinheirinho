5e7fae1d7b67562bfb1d9835e81bfa8b1f6376ae
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-11-21 08:57:12 -0300

    feat: add multiprovider

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:meu_dinheirinho/data/data_source/local_datasource.dart';
+import 'package:meu_dinheirinho/data/repositories/moviment_repository.dart';
+import 'package:meu_dinheirinho/domain/use_case/create_moviment_usecase.dart';
+import 'package:meu_dinheirinho/domain/use_case/get_moviments_usecase.dart';
+import 'package:meu_dinheirinho/iu/providers/moviment_provider.dart';
 import 'package:meu_dinheirinho/iu/screens/add_wallet_moviment/add_wallet_moviment.dart';
 import 'package:meu_dinheirinho/iu/screens/moviment_details/moviment_details.dart';
 import 'package:meu_dinheirinho/iu/screens/screen_base.dart';
 import 'package:meu_dinheirinho/iu/screens/update_moviment/update_moviment_screen.dart';
+import 'package:provider/provider.dart';
 
 import 'iu/screens/wallet/wallet.dart';
 
@@ -11,16 +17,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Meu Dinheirinho",
-      initialRoute: "/base",
-      routes: {
-        "/base": (context) => const ScreenBase(),
-        "/wallet": (context) => const Wallet(),
-        "/addWalletMoviment": (context) => const AddWalletMoviment(),
-        "/movimentDetails": (context) => const MovimentDetails(),
-        "/updateMovimentScreen": (context) => const UpdateMovimentScreen()
-      },
+    final localDatasource =  LocalDatasource();
+    final movimentRepo = MovimentRepository(local: localDatasource);
+    final createMovimentUsecase = CreateMovimentUsecase(repository: movimentRepo);
+    final getMovimentsUsecase = GetMovimentsUsecase(repository: movimentRepo);
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MovimentProvider(
+          createMovimentUsecase: createMovimentUsecase,
+          getMovimentsUsecase: getMovimentsUsecase
+        ))
+      ],
+      child: MaterialApp(
+        title: "Meu Dinheirinho",
+        initialRoute: "/base",
+        routes: {
+          "/base": (context) => const ScreenBase(),
+          "/wallet": (context) => const Wallet(),
+          "/addWalletMoviment": (context) => const AddWalletMoviment(),
+          "/movimentDetails": (context) => const MovimentDetails(),
+          "/updateMovimentScreen": (context) => const UpdateMovimentScreen()
+        },
+      ),
     );
   }
 }
